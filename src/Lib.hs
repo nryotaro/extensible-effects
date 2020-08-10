c3fa002b2643dd411c6a86bebb9398be8e8a0618
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-08-11 02:00:13 +0900

    impl fmap

--- src/Lib.hs
+++ src/Lib.hs
@@ -16,6 +16,8 @@ module Lib
   ( someFunc,
   )
 where
+import Control.Monad
+import Data.Functor
 
 data VE w = Val w | E (Int -> VE w)
 
@@ -23,8 +25,27 @@ newtype Eff a = Eff
   { runEff :: forall w. (a -> VE w) -> VE w
   }
 
+instance Functor Eff where
+  fmap f (Eff c) = Eff $ \k -> c (\a -> k (f a))
+
+{-
+instance Monad Eff where
+  return x = Eff $ \k -> k x
+  m >>= f = Eff $ \k -> runEff m (\v -> runEff (f v) k)
+-}
+
 ask :: Eff Int
 ask = Eff (\k -> E k)
+
+admin :: Eff w -> VE w
+admin (Eff m) = m Val
+
+runReader :: Eff w -> Int -> w
+runReader m e = loop (admin m)
+  where
+    loop :: VE w -> w
+    loop (Val x) = x
+    loop (E k) = loop (k e)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
