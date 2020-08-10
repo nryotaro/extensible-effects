924631799e56b44066fb49b35872903de23ca136
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-08-11 00:43:39 +0900

    ask

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,30 @@
+-- {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE RankNTypes #-}
+
+--
+-- {-# LANGUAGE MultiParamTypeClasses #-}
+-- {-# LANGUAGE TypeOperators #-}
+-- {-# LANGUAGE StandaloneDeriving #-}
+-- {-# LANGUAGE DeriveDataTypeable, GeneralizedNewtypeDeriving, DeriveFunctor #-}
+-- {-# LANGUAGE FlexibleContexts #-}
+-- {-# LANGUAGE ExistentialQuantification #-}
+-- {-# LANGUAGE ScopedTypeVariables #-}
+-- {-# LANGUAGE NoMonomorphismRestriction #-}
+-- {-# LANGUAGE AllowAmbiguousTypes #-}
+
 module Lib
   ( someFunc,
   )
 where
+
+data VE w = Val w | E (Int -> VE w)
+
+newtype Eff a = Eff
+  { runEff :: forall w. (a -> VE w) -> VE w
+  }
+
+ask :: Eff Int
+ask = Eff (\k -> E k)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
